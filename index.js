11d664b31af6dbd3cd655c4a5b4338769bf3a33e
Lumin228 <103246864+Lumin228@users.noreply.github.com>
2025-03-01 21:07:56 +0000

    Deploying to gh-pages from @ Lumin228/HW_-10@51603247e5779bf1b0daf20a0ce6e1dcd1871cb2 🚀

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-import"./assets/styles-Du1tBAGb.js";import{a as o,S as s}from"./assets/vendor-C-3xrRrD.js";const t=document.querySelector(".gallery"),l=document.querySelector("form"),c="41243216-d0c3aacbdbd6d0005d29ac5a4";l.addEventListener("submit",i=>{i.preventDefault(),console.log(),o.get(`https://pixabay.com/api/?key=${c}&q=${i.currentTarget.search.value}&image_type=photo`).then(function(e){console.log(e.data.hits),e.data.hits.map(a=>{t.insertAdjacentHTML("afterbegin",`<a href="${a.largeImageURL}" class="smallIMG">
-            <img src="${a.previewURL}" class="bigIMG" height="100" width="150">
+import"./assets/styles-COGGUw73.js";import{a as c,S as r,i as d}from"./assets/vendor-cW5AzyDy.js";const p=document.querySelector(".gallery"),h=document.querySelector("form"),n="41243216-d0c3aacbdbd6d0005d29ac5a4",t=document.querySelector(".load_MORE");let s="",l=1;console.log(t);h.addEventListener("submit",a=>{a.preventDefault(),s=a.currentTarget.search.value,console.log(),c.get(`https://pixabay.com/api/?key=${n}&q=${a.currentTarget.search.value}&image_type=photo&page=1`).then(function(o){if(console.log(o.data.hits.length),o.data.hits.length===0)throw error;o.data.hits.map(i=>{p.insertAdjacentHTML("afterbegin",`<a href="${i.largeImageURL}" class="smallIMG">
+            <img src="${i.previewURL}" class="bigIMG" height="100" width="150">
             <div class='info'>
             <div class='elem'>
             <p>Likes</p>
-            <p>${a.likes}</p>
+            <p>${i.likes}</p>
             </div>
             <div class='elem'>
             <p>Views</p>
-            <p>${a.views}</p>
+            <p>${i.views}</p>
             </div>
             <div class='elem'>
             <p>Comments</p>
-            <p>${a.comments}</p>
+            <p>${i.comments}</p>
             </div>
             <div class='elem'>
             <p>Downloads</p>
-            <p>${a.downloads}</p>
+            <p>${i.downloads}</p>
             </div>
             </div>
-            </a>`)}),new s(".gallery a").on("show.simplelightbox",function(){})}).catch(function(e){console.log(e)})});
+            </a>`)}),t.classList.remove("is-hidden"),new r(".gallery a").on("show.simplelightbox",function(){})}).catch(function(o){d.warning({title:"Caution",message:`We don't have a photos of ${s}`})})});t.addEventListener("click",()=>{l+=1,console.log(),c.get(`https://pixabay.com/api/?key=${n}&q=${s}&image_type=photo&page=${l}`).then(function(a){if(console.log(a.data.hits.length),a.data.hits.length===0)throw error;a.data.hits.map(e=>{p.insertAdjacentHTML("beforeend",`<a href="${e.largeImageURL}" class="smallIMG">
+            <img src="${e.previewURL}" class="bigIMG" height="100" width="150">
+            <div class='info'>
+            <div class='elem'>
+            <p>Likes</p>
+            <p>${e.likes}</p>
+            </div>
+            <div class='elem'>
+            <p>Views</p>
+            <p>${e.views}</p>
+            </div>
+            <div class='elem'>
+            <p>Comments</p>
+            <p>${e.comments}</p>
+            </div>
+            <div class='elem'>
+            <p>Downloads</p>
+            <p>${e.downloads}</p>
+            </div>
+            </div>
+            </a>`)}),new r(".gallery a").on("show.simplelightbox",function(){}),scrollBy({top:600,behavior:"smooth"})}).catch(function(a){d.warning({title:"Caution",message:`We don't have a photos of ${s}`})})});
 //# sourceMappingURL=index.js.map
